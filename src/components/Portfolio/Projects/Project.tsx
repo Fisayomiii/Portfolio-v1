7f0c60ca4905ba39accbdff09a186799d7bb27dd
oluwafisayomi <wrld26993@gmail.com>
2023-01-24 23:22:23 +0100

    [Feat] In the portfolio section i widened the cards

--- src/components/Portfolio/Projects/Project.tsx
+++ src/components/Portfolio/Projects/Project.tsx
@@ -6,19 +6,19 @@ function Project() {
         {
             img: "https://i.ibb.co/3WQhSKF/ubivid.jpg",
             title: "Ubivid",
-            desc: "✨ With Ubivid Users can come stream movies to maybe gain information about a specific movie about to be released or even a movie realsed.",
+            desc: "✨ With Ubivid Users can come stream movies to gain information about a specific movie about to be released or even a movie released.",
             link: "https://ubivid.vercel.app",
         },
         {
             img: "https://i.ibb.co/k8Q6Tw7/tailui.jpg",
             title: "TailUI",
-            desc: "✨ TailUI a collection of free Tailwind CSS components to bootstrap your new apps, projects or landing sites!",
+            desc: "✨ TailUI a collection of Free Tailwind CSS components to bootstrap your new apps, projects or landing sites!",
             link: "https://tailui.vercel.app",
         },
         {
             img: "https://i.ibb.co/DbY8VPL/earlybean.jpg",
             title: "EarlyBean",
-            desc: "✨ EarlyBean is a web application that help children save money better. (it's just a landing page built with tailwindCss and vite)",
+            desc: "✨ EarlyBean is a web application that help children save money better.",
             link: "https://earlybean.vercel.app",
         },
         {
